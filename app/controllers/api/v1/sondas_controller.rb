500c3aba977c64017fb9c715944890e3352a3e42
dell <vagner@envixo.com>
2020-12-19 15:11:49 -0300

    Exibe coordenadas e face atuais

--- app/controllers/api/v1/sondas_controller.rb
+++ app/controllers/api/v1/sondas_controller.rb
@@ -1,3 +1,12 @@
 class Api::V1::SondasController < ApplicationController
+  before_action :set_sonda, only: [:show]
+
+  def show
+  end
+
+  private
+  def set_sonda
+    @sonda = Sonda.find(params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :sondas, only: [:show], defaults: { format: 'json'}
+  namespace :api do
+    namespace :v1 do
+      resources :sondas, only: [:show], defaults: { format: 'json'}
+    end
+  end
 end
