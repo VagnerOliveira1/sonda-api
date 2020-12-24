44e2a74f9ee7bcf729a033bd7a48719679402112
dell <vagner@envixo.com>
2020-12-23 22:31:33 -0300

    Inicia refectoring

--- app/models/sonda.rb
+++ app/models/sonda.rb
@@ -4,39 +4,24 @@ class Sonda < ApplicationRecord
   validates_inclusion_of :coordinate_y, :in => 0..4
 
   def self.move(commands)
-    mensagem = []
-    cont_y = cont_x = 0
-    eixo = " "
+    @cont_y = @cont_x = 0
+    loop_commands(commands)
+    @sonda
+  end
+
+  def self.loop_commands(commands)
+    @message= " "
     commands.each_with_index do |c, index|
       if c!= 'M'
-        if c == 'GE'
-          cont_x = cont_y = 0
-          mensagem << " Girou para Esquerda "
-          set_face(c)
-        elsif c == 'GD'
-          cont_x = cont_y = 0
-          mensagem << " girou para Direita"
-          set_face(c)
-        else
-          puts "comando inválido"
-        end
+        get_direction(c)
       else
         set_coordinates
-        if @sonda.face == 'C' || @sonda.face == 'B'
-          cont_y += 1
-        else
-          cont_x += 1
-        end
+        get_axis
       end
       if c =='M' && commands[index+1] != 'M'
-        if cont_x > 0 || cont_y > 0
-          maior = cont_y > cont_x ? cont_y : cont_x
-          eixo =(@sonda.face.include?('D') || @sonda.face.include?('E')) ? " X " : " Y "
-          mensagem << " e andou #{maior} posições no eixo #{eixo}"
-        end
+        create_message
       end
     end
-    #@sonda.sonda.commands.reduce.to_s.delete("[]")
   end
 
   def self.set_face(c)
@@ -46,7 +31,6 @@ class Sonda < ApplicationRecord
     when face.include?('E')
       if c.include?('E')
         face = 'C'
-        #girou para a esquerda
       elsif c.include?('D')
         face = 'B'
       end
@@ -73,6 +57,20 @@ class Sonda < ApplicationRecord
     @sonda.save!
   end
 
+  def self.get_direction(c)
+    if c == 'GE'
+      @cont_x = @cont_y = 0
+      @message+= " Girou para Esquerda "
+      set_face(c)
+    elsif c == 'GD'
+      @cont_x = @cont_y = 0
+      @message+= " girou para Direita"
+      set_face(c)
+    else
+      puts "comando inválido"
+    end
+  end
+
   def self.set_coordinates
     x = y = 0
     face = @sonda.face
@@ -95,6 +93,22 @@ class Sonda < ApplicationRecord
     commands = commands.reduce.split(",").map { |co| co.delete("\"").strip}
     if commands.uniq.size == 3
       move(commands)
+    end
+  end
+
+  def self.get_axis
+    if @sonda.face == 'C' || @sonda.face == 'B'
+      @cont_y += 1
+    else
+      @cont_x += 1
+    end
+  end
+
+  def self.create_message
+    if @cont_x > 0 || @cont_y > 0
+      maior = @cont_y > @cont_x ? @cont_y : @cont_x
+      axis =(@sonda.face.include?('D') || @sonda.face.include?('E')) ? " X " : " Y "
+      @message+=" e andou #{maior} posições no eixo #{axis}"
     end
   end
 end
